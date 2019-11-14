811560082c7a0b0e86e03a3be280a1716e1be32a
Nick Spreitzer <nick@200-ok.com>
2019-11-14 15:37:58 -0600

    Update open graph images

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -90,8 +90,8 @@ const siteConfig = {
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
+  ogImage: 'img/notebook.svg',
+  twitterImage: 'img/notebook.svg',
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
